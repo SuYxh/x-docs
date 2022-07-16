e4c90f7df47b1341c9bb938a3ab71f5456a6c2f6
SuYxh <46957352+SuYxh@users.noreply.github.com>
2022-07-16 11:50:50 +0000

    Deploying to gh-pages from @ SuYxh/x-docs@d871913c301a862256c3693e7cf21a47756ce7de 🚀

---
+++ assets/project_index.md.2722f9ba.js
@@ -0,0 +1,1 @@
+import{o as t,c as e,a as r}from"./app.7e0da940.js";const a='{"title":"工程化","description":"","frontmatter":{},"relativePath":"project/index.md","lastUpdated":1657972228289}',d={},n=[r("h1",{id:"工程化"},"工程化",-1),r("p",null,"工程化的东西",-1)];d.render=function(r,a,d,o,i,p){return t(),e("div",null,n)};export default d;export{a as __pageData};
